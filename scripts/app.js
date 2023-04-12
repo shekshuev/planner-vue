b666389ef9ee5f352cfca22a236402836a80f5d3
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2023-04-12 15:14:59 +0300

    pwa fix

--- scripts/app.js
+++ scripts/app.js
@@ -42,13 +42,11 @@ app.mount("#app");
 
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function () {
-        navigator.serviceWorker.register("/scripts/sw.js").then(
+        navigator.serviceWorker.register("/sw.js").then(
             function (registration) {
-                // Registration was successful
                 console.log("ServiceWorker registration successful with scope: ", registration.scope);
             },
             function (err) {
-                // registration failed :(
                 console.log("ServiceWorker registration failed: ", err);
             }
         );
--- scripts/sw.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-importScripts("sw-toolbox.js");
-toolbox.precache(["../index.html", "../styles/style.css"]);
-toolbox.router.get("../assets/*", toolbox.cacheFirst);
-toolbox.router.get("../*", toolbox.networkFirst, { networkTimeoutSeconds: 5 });
