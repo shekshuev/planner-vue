507f611e26984044d88259bfebaabe4d3cd84206
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2023-04-15 09:14:51 +0300

    logo link removed

--- scripts/components/top-menu.js
+++ scripts/components/top-menu.js
@@ -5,7 +5,7 @@ export const TopMenu = {
     template: /*html*/ `
         <nav class="bg-white border-gray-200">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="https://flowbite.com/" class="flex items-center">
+                <a href="#" class="flex items-center">
                     <img src="assets/icons/icon-128x128.png" class="h-8 mr-3" alt="Flowbite Logo" />
                     <span class="self-center text-2xl font-semibold whitespace-nowrap">Планирование ВКС</span>
                 </a>
